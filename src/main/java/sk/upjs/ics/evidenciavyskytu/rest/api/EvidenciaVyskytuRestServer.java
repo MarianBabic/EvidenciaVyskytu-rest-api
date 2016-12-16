415bfbb4efdcec27e68193f667d09d5254257ff9
student <student@158.197.35.85>
2016-12-16 07:26:17 +0100

    16.12.

---
+++ src/main/java/sk/upjs/ics/evidenciavyskytu/rest/api/EvidenciaVyskytuRestServer.java
@@ -0,0 +1,15 @@
+package sk.upjs.ics.evidenciavyskytu.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvidenciaVyskytuRestServer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EvidenciaVyskytuRestServer.class, args);
+
+    }
+
+}
